06c76aa04b143276d12706c0b61b592ce149deea
FarhadJ83 <0137722@student.uow.edu.my>
2025-08-18 20:57:55 +0800

    Minor fixes

--- Assets/Tap.cs
+++ Assets/Tap.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Tap : MonoBehaviour
 {
@@ -19,7 +22,30 @@ public class Tap : MonoBehaviour
     {
         // On tap, load the Menu Scene if the tap is not on a button
 
+        if (IsTapOverButtonOrPanel(e))
+            return;
+
         SceneManager.LoadScene("LevelsScene");
     }
+
+    private bool IsTapOverButtonOrPanel(TouchGesture e)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = e.ScreenPosition;
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.GetComponent<Button>() != null || result.gameObject.tag.Equals("Panel"))
+            {
+                return true;
+
+            }
+        }
+        return false;
+    }
+
 
 }
